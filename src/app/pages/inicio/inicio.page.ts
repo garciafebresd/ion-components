faf57710fe1d9e72fb0ff4573042de6f1464a238
daniel garcia <dgarcia@position.cl>
2020-03-11 13:05:45 -0300

    add ion-list

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
+  components: Component[] = [
+    {
+      icon: 'alert-circle-outline',
+      name: 'alert',
+      redirecTo: '/alert'
+    },
+    {
+      icon: 'american-football',
+      name: 'action sheet',
+      redirecTo: '/action-sheet'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+interface Component {
+  icon: string;
+  name: string;
+  redirecTo: string;
 }
